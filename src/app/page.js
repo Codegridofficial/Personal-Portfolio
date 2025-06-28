58afdecd356d755dd463a27740ad818d50ca8b7b
Dinand <codegridofficial@gmail.com>
2025-06-28 12:14:16 +0200

    Version 1

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+'use client';
+import Test from "../components/Test.js";
+import test from "../components/Test.js"
+
+export default function Home() {
+  return (
+    <div>
+      <Test></Test>
+    </div>
+  );
+}
